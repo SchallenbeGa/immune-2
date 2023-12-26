5be661f8acc608d54ba136918a06e1bc7cf0fb65
Gabriel <gabriel.schallenberger@outlook.com>
2023-12-26 18:31:50 +0100

    .

--- resources/views/sign-in/partials/form-fields.blade.php
+++ resources/views/sign-in/partials/form-fields.blade.php
@@ -1,10 +1,10 @@
 @csrf
 <fieldset class="form-group">
-  <input type="text" id="sign-in-email" style="margin:10px" class="form-control form-control-lg" name="email" placeholder="Email" value="{{ (isset($oldEmail) ? $oldEmail : old('email')) }}">
+  <input type="text" id="sign-in-email" style="margin-top:10px" class="form-control form-control-lg" name="email" placeholder="Email" value="{{ (isset($oldEmail) ? $oldEmail : old('email')) }}">
 </fieldset>
 <fieldset class="form-group">
-  <input type="password" id="sign-in-password" style="margin:10px" class="form-control form-control-lg" name="password" placeholder="Password">
+  <input type="password" id="sign-in-password" style="margin-top:10px" class="form-control form-control-lg" name="password" placeholder="Password">
 </fieldset>
-<button class="btn btn-lg btn-primary pull-xs-right" style="margin:10px">
+<button class="btn btn-lg btn-primary pull-xs-right" style="margin-top:10px">
   Sign in
 </button>
--- resources/views/sign-up/partials/form-fields.blade.php
+++ resources/views/sign-up/partials/form-fields.blade.php
@@ -1,13 +1,13 @@
 @csrf
 <fieldset class="form-group">
-  <input id="sign-up-username" style="margin:10px" class="form-control form-control-lg" type="text" name="username" placeholder="Username" value="{{ (isset($oldUsername) ? $oldUsername : old('username')) }}">
+  <input id="sign-up-username" style="margin-top:10px" class="form-control form-control-lg" type="text" name="username" placeholder="Username" value="{{ (isset($oldUsername) ? $oldUsername : old('username')) }}">
 </fieldset>
 <fieldset class="form-group">
-  <input id="sign-up-email" style="margin:10px" class="form-control form-control-lg" type="text" name="email" placeholder="Email" value="{{ (isset($oldEmail) ? $oldEmail : old('email')) }}">
+  <input id="sign-up-email" style="margin-top:10px" class="form-control form-control-lg" type="text" name="email" placeholder="Email" value="{{ (isset($oldEmail) ? $oldEmail : old('email')) }}">
 </fieldset>
 <fieldset class="form-group">
-  <input id="sign-up-password" style="margin:10px" class="form-control form-control-lg" type="password" name="password" placeholder="Password">
+  <input id="sign-up-password" style="margin-top:10px" class="form-control form-control-lg" type="password" name="password" placeholder="Password">
 </fieldset>
-<button class="btn btn-lg btn-primary pull-xs-right" style="margin:10px">
+<button class="btn btn-lg btn-primary pull-xs-right" style="margin-top:10px">
   Sign up
 </button>
