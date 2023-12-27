d1ceb06a1ab245f78835e508e3b87eb25a8accc6
Gabriel <gabriel.schallenberger@outlook.com>
2023-12-27 17:44:05 +0100

    Update trade-preview.blade.php

--- resources/views/home/partials/trade-preview.blade.php
+++ resources/views/home/partials/trade-preview.blade.php
@@ -4,7 +4,7 @@
     <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top" href="/symbol/{{ $entry->name }}" hx-push-url="/symbol/{{ $entry->name }}" hx-get="/htmx/symbol/{{ $entry->name }}" hx-target="#app-body" class="preview-link">
       
       <div class="col-lg-8">
-        <h6 class="mb-0">{{ $entry->name }} - {{ $entry->side}} at {{ $entry->price}}</h6>
+        <h5 class="mb-0">{{ $entry->name }} - {{ $entry->side}} at {{ $entry->price}}</h5>
         <small class="text-body-secondary">{{ $entry->updated_at }}</small>
       </div>
     </a>
