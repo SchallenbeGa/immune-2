8084addbdc7b5126543aaf5f7a402b6a0bbb9075
Gabriel <gabriel.schallenberger@outlook.com>
2024-03-04 17:07:57 +0100

    w

--- portfolio_server-radvisor_plus/bot.py
+++ portfolio_server-radvisor_plus/bot.py
@@ -51,7 +51,7 @@ def order(pair_id,pair,limit,side):
     print(config('FUTURE'))
     try:
         # place limit order
-        if config('FUTURE'):
+        if config('FUTURE') == 'True':
             client.futures_change_leverage(symbol=pair, leverage=config('FUTURE_LEVERAGE'))
             if config('FUTURE_COIN'): # COIN-M wip
                 order = client.futures_coin_create_order(
