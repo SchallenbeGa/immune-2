8761d4cd78b994e8b434415964d5eb03b4923e69
Gabriel <gabriel.schallenberger@outlook.com>
2024-09-25 21:04:42 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
     <meta
   name="description"
   content="gabriel0's simple recipe for maple donuts
