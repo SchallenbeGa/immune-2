0d14c875b593efa2eb57ef89f475d1940009a500
Gabriel <gabriel.schallenberger@outlook.com>
2024-09-25 22:01:59 +0200

    w

--- resources/views/articles/partials/edit-button.blade.php
+++ resources/views/articles/partials/edit-button.blade.php
@@ -1,8 +1,8 @@
-<button style="margin:0.2rem;" class="btn btn-outline-secondary btn-sm edit-button"
-  hx-get="/htmx/editor/{{ $article->slug }}"
+<a style="margin:0.2rem;" class="btn btn-outline-secondary btn-sm edit-button"
+  href="/editor/{{ $article->slug }}"
   hx-target="#app-body"
   hx-push-url="/editor/{{ $article->slug }}"
 >
   <i class="ion-edit"></i>
   Edit Article
-</button>
+</a>
