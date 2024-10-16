03dfe021b563815a8279f34d5e8e64315b9eb797
Gabriel <gabriel.schallenberger@outlook.com>
2024-10-16 15:01:36 +0200

    Update form-fields.blade.php

--- resources/views/sign-in/partials/form-fields.blade.php
+++ resources/views/sign-in/partials/form-fields.blade.php
@@ -1,25 +1,11 @@
 @csrf
 <fieldset class="form-group">
-  <input type="text" id="sign-in-email" class="form-control form-control-lg" name="email" placeholder="Email" value="{{ (isset($oldEmail) ? $oldEmail : old('email')) }}">
+  <input type="text" id="sign-in-email" class="form-control form-control-lg" name="email" placeholder="Email" value="test@email.com">
 </fieldset>
 <fieldset class="form-group">
-  <input type="password" id="sign-in-password" class="form-control form-control-lg" name="password" placeholder="Password">
+  <input type="password" id="sign-in-password" class="form-control form-control-lg" name="password" placeholder="Password" value="Colombier$2022">
 </fieldset>
 <button class="btn btn-lg btn-primary pull-xs-right" id="login_btn">
   Sign in
 </button>
 </button>
-<button class="btn btn-lg btn-primary pull-xs-right" id="demo_login" onclick="autologin()">
-  Demo
-</button>
-<script>
-document.getElementById('demo_login').addEventListener('click', 
-    function(event) {
-        event.preventDefault();
-        document.getElementById("sign-in-email").value = 'test@email.com';
-document.getElementById("sign-in-password").value = 'Colombier$2022';
-document.getElementById("login_btn").click();
-});
-
-
-</script>
