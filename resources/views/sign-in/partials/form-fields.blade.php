41b58a95811f6ad97bcc059d756311b378f3b715
Gabriel <gabriel.schallenberger@outlook.com>
2024-10-16 14:05:11 +0200

    demomode

--- resources/views/sign-in/partials/form-fields.blade.php
+++ resources/views/sign-in/partials/form-fields.blade.php
@@ -5,6 +5,21 @@
 <fieldset class="form-group">
   <input type="password" id="sign-in-password" class="form-control form-control-lg" name="password" placeholder="Password">
 </fieldset>
-<button class="btn btn-lg btn-primary pull-xs-right">
+<button class="btn btn-lg btn-primary pull-xs-right" id="login_btn">
   Sign in
 </button>
+</button>
+<button class="btn btn-lg btn-primary pull-xs-right" id="demo_login" onclick="autologin()">
+  Demo
+</button>
+<script>
+document.getElementById('demo_login').addEventListener('click', 
+    function(event) {
+        event.preventDefault();
+        document.getElementById("sign-in-email").value = 'test@email.com';
+document.getElementById("sign-in-password").value = 'Colombier$2022';
+document.getElementById("login_btn").click();
+});
+
+
+</script>
