3d30e962ad8e6f92556985dd661b70ee66bbfc18
Gabriel <gabriel.schallenberger@outlook.com>
2024-10-24 20:19:16 +0200

    ref

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,10 +39,22 @@
     <script src="{{ asset('js/htmx.js') }}"></script>
     <script src="{{ asset('js/htmx-head-support.js') }}"></script>
     <script>
-      $("a").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
+     
+     function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+// Délégation d'événement : On attache l'écouteur sur le conteneur parent
+document.getElementById("app-body").addEventListener("click", function(event) {
+    if (event.target.tagName === "H1") {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+        scrollToTop(); // Appelle la fonction pour défiler vers le haut
+    }
 });
+
     // Get the session lifetime (in minutes) from your backend and multiply by 60 to convert to seconds.
     var sessionLifetimeInMinutes = {{ config('session.lifetime') }}; // Insert backend value here
     var sessionLifetimeInSeconds = sessionLifetimeInMinutes * 60; // Convert to seconds
